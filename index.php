81bd415892ec484f1818c5eb0872cf18252776da
ifs20026 <ifs20026@students.del.ac.id>
2022-09-28 20:54:29 +0700

    Praktikum Week 5

--- index.php
+++ index.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>PABWE - ITDEL</title>
-    <link href="assets/vendor/bootstrap5/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
+    <link href="assets/vendor/bootstrap5/css/bootstrap.min.css" rel="stylesheet" />
+  </head>
+  <body>
     <div class="continer">
-        <div class="card">
-            <div class="card-body w-50 mx-auto">
+      <div class="card">
+        <div class="card-body w-50 mx-auto">
+          <img src="./assets/img/foto1.jpg" alt="foto-saya" class="img-thumbnail d-block mx-auto" width="150" />
+          <h3 class="text-center">11S20026_Ivanston Simbolon</h3>
 
-                <img src="./assets/img/foto1.jpg" alt="foto-saya" class="img-thumbnail d-block mx-auto" width="150"/>
-                <h3 class="text-center">11S20026_Ivanston Simbolon</h3>
-
-                <div class="list-group">
-                    <li href="#" class="list-group-item list-group-item-action active" aria-current="true">
-                      <h3>Hasil Praktikum</h3>
-                    </li>
-                    <a href="./minggu2/profil.html" class="list-group-item list-group-item-action">Minggu Ke-2</a>
-                    <a href="./minggu3/loginval.html" class="list-group-item list-group-item-action">Minggu Ke-3</a>
-                    <a href="./minggu4/index.html" class="list-group-item list-group-item-action">Minggu Ke-4</a>
-                </div>
-            </div>
+          <div class="list-group">
+            <li href="#" class="list-group-item list-group-item-action active" aria-current="true">
+              <h3>Hasil Praktikum</h3>
+            </li>
+            <a href="./minggu2/profil.html" class="list-group-item list-group-item-action">Minggu Ke-2</a>
+            <a href="./minggu3/loginval.html" class="list-group-item list-group-item-action">Minggu Ke-3</a>
+            <a href="./minggu4/index.html" class="list-group-item list-group-item-action">Minggu Ke-4</a>
+            <a href="./minggu5/dom1.html" class="list-group-item list-group-item-action">Minggu Ke-5</a>
+          </div>
         </div>
+      </div>
     </div>
-</body>
+  </body>
 </html>
